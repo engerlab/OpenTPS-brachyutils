fdc52ae1c236939e6e6d9df0ec194c71578aa7e1
sowuy <sophie.wuyckens@uclouvain.be>
2021-06-10 16:58:31 +0200

    include call to scanAlgo in BDT class

--- Process/jsonPlan.py
+++ Process/jsonPlan.py
@@ -1,5 +1,7 @@
 import json
 from Process.RTplan import *
+import requests
+import csv
 
 '''
 Class to convert openTPS plan into json plan (input for scanAlgo) 
@@ -13,23 +15,96 @@ file_path = "Plan_OpenTPS.tps"
 plan = RTplan()
 plan.load(file_path)
 
-jsonPlans = JsonPlans(plan)
+# Create BDT object
+BDT = BDT(plan)
 # Create 1 json file per beam
-jsonPlans.allBeams()
+jsonFiles = BDT.create_jsonFiles()
+# call scanAlgo to get times (in ms)
+BDT.call_scanAlgo(jsonFiles)
+# create inputCSV for arcAlgo
+BDT.create_inputCSV("yo.csv")
+
 
 '''
 
 
-
-class JsonPlans:
-    def __init__(self,plan, Gantry = "PPlus"):
+class BDT:
+    def __init__(self,plan, config):
         self.openTPSPlan = plan
-        self.Gantry = Gantry
-        
-    def allBeams(self):
+        self.params = self.get_config_params(config)
+        self.Gantry = self.params['Gantry']
+        self.url = self.params['Gateaway']
+        self.inputCSV = []
+
+                
+    def create_jsonFiles(self):
+        jsonFiles = []
         for i,beam in enumerate(self.openTPSPlan.Beams):
             jsonPlan = JsonPlan(self.openTPSPlan, self.Gantry,i)
-            jsonPlan.save("beam_" + str(i) + ".json")
+            filename = "beam_" + str(i) + ".json"
+            jsonPlan.save(filename)
+            jsonFiles.append(filename)
+        return jsonFiles
+
+
+    def get_config_params(self,config):
+        with open(config) as f:
+            lines = f.readlines()
+            columns = []
+            i = 1
+            d = {}
+            for line in lines:
+                if line.strip():
+                    line = line.strip().split(',')# remove leading/trailing white spaces
+                    key  = line[0].strip()
+                    value  = line[1].strip()
+                    d[key]=value
+        return d
+
+
+    def call_scanAlgo(self,jsonFilesList):
+        previous_energy = 0
+        for index,beam in enumerate(jsonFilesList):
+            with open(beam,'rb') as f:
+                data = json.load(f)
+            gantry_angle = data['gantryangle']
+            scanAlgo = requests.post(self.url,json=data).json()
+            if 'cause' in scanAlgo:
+               print("!!!!!!!!! ScanAlgo ERROR in beam !!!!!!! ", index)  
+               print('\n')
+               print(scanAlgo['cause'])
+               print('\n')
+            else:
+                print('BDT {}  = {} '.format(index,scanAlgo['layer'][-1]['spot'][-1]['start']))
+                print('\n')
+                angle_time = 300 + (scanAlgo['layer'][-1]['spot'][-1]['start'] + scanAlgo['layer'][-1]['spot'][-1]['duration'] - scanAlgo['layer'][0]['spot'][0]['start'])
+                beam_csv = []
+                if previous_energy == 0:
+                    beam_csv.append(str(gantry_angle))
+                    beam_csv.append(str(angle_time))
+                    beam_csv.append(str('NA'))
+                elif scanAlgo['layer'][0]['energy'] < previous_energy:
+                    beam_csv.append(str(gantry_angle))
+                    beam_csv.append(str(angle_time))
+                    beam_csv.append(str('Down'))
+                else:
+                    beam_csv.append(str(gantry_angle))
+                    beam_csv.append(str(angle_time))
+                    beam_csv.append(str('Up'))
+                previous_energy = scanAlgo['layer'][-1]['energy']
+                self.inputCSV.append(beam_csv)
+    
+    def create_inputCSV(self,inputCSV_filename):
+        header = ['Angle','Time','Switch']
+        with open(inputCSV_filename, 'w') as f:
+            # create the csv writer
+            writer = csv.writer(f)    
+            # write the header
+            writer.writerow(header)
+            writer.writerows(self.inputCSV)
+        
+    def call_arcAlgo(self):
+        pass
 
 
 
@@ -71,6 +146,7 @@ class JsonPlan:
             self.actualTemperature = "20.0"
             self.referenceTemperature = "20.0"
             self.actualPressure = "101.325"
+#file_path = "data/Phantoms/phantom_3mm/OpenTPS/Plan_phantom_1mm_9Beams_LS5_SS5_RTV7-5_Mai-26-2021_09-48-33.tps"
             self.referencePressure = "101.325"
             self.doseCorrectionFactor = "1"
             self.icOffsetX = "0"
