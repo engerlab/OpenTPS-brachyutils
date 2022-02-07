ddc0ea4776de02bd44472060a8b0676847907f4e
damien <damien.dasnoy@gmail.com>
2022-02-07 13:01:01 +0100

    sdfgdg

--- Script/startScript.py
+++
@@ -1,9 +0,0 @@
-from Core.Data.dynamic3DSequence import Dynamic3DSequence
-from Core.Data.patient import Patient
-from Core.Data.patientData import PatientData
-from Core.IO.dataLoader import loadData
-from Core.api import API
-
-loadData(API.patientList,['/home/sylvain/Documents/4DCT/4DCT/p20','/home/sylvain/Documents/4DCT/4DCT/p30','/home/sylvain/Documents/4DCT/4DCT/p40'])
-
-
